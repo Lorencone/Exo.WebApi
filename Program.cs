1f35fc1076bdeff1c5de1379ee5474ef8cbd659a
Lorencone <mmlorencone@gmail.com>
2025-11-24 16:04:46 -0300

    Correções nos arquivos do Program.cs
    Correção na parte de login, para gerar o token

--- Program.cs
+++ Program.cs
@@ -2,24 +2,19 @@ using Exo.WebApi.Contexts;
 using Exo.WebApi.Repositories;
 using Microsoft.IdentityModel.Tokens;
 
-
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddScoped<ExoContext>(); 
 // Registra o repositório
 builder.Services.AddTransient<ProjetoRepository>(); 
 builder.Services.AddControllers();
-builder.Services.AddTransient<UsuarioRepository>();
-var app = builder.Build();
-
-//Parte do Login
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddAuthentication(options =>
 {
-    options.DefaultAuthenticateScheme = "JwtBearer";
-    options.DefaultChallengeScheme = "JwtBearer";
-}).AddJwtBearer("JwtBearer", options =>
+    options.DefaultAuthenticateScheme = "Bearer";
+    options.DefaultChallengeScheme = "Bearer";
+}).AddJwtBearer("Bearer", options =>
 {
     options.TokenValidationParameters = new TokenValidationParameters
     {
@@ -28,10 +23,13 @@ builder.Services.AddAuthentication(options =>
         ValidateLifetime = true,
         IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chave-autenticacao")),
         ClockSkew = TimeSpan.FromMinutes(30),
-        ValidIssuer = "Exo.WebApi",
-        ValidAudience = "Exo.WebApi"
+        ValidIssuer = "exo.WebApi",
+        ValidAudience = "exo.WebApi"
     };
 });
+
+builder.Services.AddTransient<UsuarioRepository>();
+var app = builder.Build();
 
 // --- 2. Configuração do Pipeline HTTP ---
 
--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a178514701f0673ade0dfd17215ba4f7ad34b9b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+179f5422adc4c64bbc298acd20d0c4bf4224145a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
