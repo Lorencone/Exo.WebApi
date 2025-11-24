179f5422adc4c64bbc298acd20d0c4bf4224145a
Lorencone <mmlorencone@gmail.com>
2025-11-24 15:59:09 -0300

    Login no Programs
    Implementação do crud na programs.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Exo.WebApi.Contexts;
 using Exo.WebApi.Repositories;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +13,26 @@ builder.Services.AddControllers();
 builder.Services.AddTransient<UsuarioRepository>();
 var app = builder.Build();
 
+//Parte do Login
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "JwtBearer";
+    options.DefaultChallengeScheme = "JwtBearer";
+}).AddJwtBearer("JwtBearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chave-autenticacao")),
+        ClockSkew = TimeSpan.FromMinutes(30),
+        ValidIssuer = "Exo.WebApi",
+        ValidAudience = "Exo.WebApi"
+    };
+});
+
 // --- 2. Configuração do Pipeline HTTP ---
 
 if (app.Environment.IsDevelopment())
@@ -24,6 +44,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
